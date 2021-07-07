048ed173f7784df7607ae7585bb9255315bf9fa9
wolver <gn_wolver@hotmail.com>
2021-07-07 18:30:47 -0500

    first commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAy9MeERNRaEFf-tfvYRtNi2RNCNKLCmFs",
+    authDomain: "autendwx.firebaseapp.com",
+    projectId: "autendwx",
+    storageBucket: "autendwx.appspot.com",
+    messagingSenderId: "752505559512",
+    appId: "1:752505559512:web:06c8671cacc5c260707aad",
+    measurementId: "G-9LBRLNXQWC"
+});
